4d27f60e76bc8e36bcd7773f1514db6f2a595286
Danysoftdev <danysoftdev@gmail.com>
2025-04-08 22:49:18 -0500

    Subiendo estructura del proyecto

---
+++ src/main/java/com/eam/microservicio_spring_mariadb/MicroservicioSpringMariadbApplication.java
@@ -0,0 +1,13 @@
+package com.eam.microservicio_spring_mariadb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicioSpringMariadbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicioSpringMariadbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eam/microservicio_spring_mariadb/MicroservicioSpringMariadbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eam.microservicio_spring_mariadb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicioSpringMariadbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
